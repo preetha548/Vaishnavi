9ef6eecc3e9482377fb7b64ce73e434213b42353
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-04-01 15:51:35 +0530

    Add files via upload

---
+++ Eggless rosemilk cake/script.js
@@ -0,0 +1,12 @@
+function showRecipe(){
+
+var recipe = document.getElementById("recipe");
+
+if(recipe.style.display === "block"){
+recipe.style.display = "none";
+}
+else{
+recipe.style.display = "block";
+}
+
+}
