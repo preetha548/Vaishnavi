095889fea4b835f1e68a408b9549df1a616a6b6d
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-07-28 10:27:55 +0530

    Add files via upload

---
+++ Freedom fighters/script.js
@@ -0,0 +1,19 @@
+function showMessage() {
+
+    const message = document.getElementById("message");
+
+    message.innerHTML = `
+    🇮🇳 <br><br>
+    <h2>Salute to Our Freedom Fighters</h2>
+
+    <p>
+    Their courage, sacrifice, and determination gave us the freedom we enjoy today.
+    Let us remember and honor their invaluable contribution to our nation.
+    </p>
+
+    <br>
+
+    <h2>Jai Hind! 🇮🇳</h2>
+    `;
+
+}
